df7e49d535d8dc54ca92d2e1de122b3fa297f548
compte stagiaire <stagiaire@neopolis-dev.com>
2024-10-03 22:05:24 +0200

    first commit

--- src/app/add-produit/add-produit.component.ts
+++ src/app/add-produit/add-produit.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
 
 @Component({
   selector: 'app-add-produit',
-  templateUrl: './add-produit.component.html',
-  styleUrl: './add-produit.component.css'
+  templateUrl: './add-produit.component.html'
 })
-export class AddProduitComponent {
+export class AddProduitComponent implements OnInit {
+
+  newProduit = new Produit();
+  constructor(private produitService: ProduitService) { }
+
+  ngOnInit(): void {
+  }
+
+  addProduit() {
+    //console.log(this.newProduit);
+    this.produitService.ajouterProduit(this.newProduit);
+  }
 
 }
--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
 
 @Component({
   selector: 'app-produits',
-  templateUrl: './produits.component.html',
-  styleUrl: './produits.component.css'
+  templateUrl: './produits.component.html'
 })
-export class ProduitsComponent {
-  
-  produits : string[]; 
-  constructor (){
-    this.produits = ["PC Asus", "Imprimante Epson", "Tablette Samsung","PC MSI"];
+export class ProduitsComponent implements OnInit {
+
+    produits? : Produit[]; //un tableau de produits
+
+  constructor(private produitService: ProduitService) {
+   //this.produits=[];
+     }
+
+  ngOnInit(): void {
+
+    this.produits = this.produitService.listeProduits();
   }
+
+  supprimerProduit(p: Produit)
+    {
+     // console.log(p);
+      let conf = confirm("Etes-vous sûr ?");
+      if (conf)
+        this.produitService.supprimerProduit(p);
+    } 
 
 }
